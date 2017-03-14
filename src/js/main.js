739b94aa1c206e0929c491c8acd2d9041293c84a
jiangrun <381265457@qq.com>
2017-03-14 18:32:27 +0800

    2017/03/14响应式学习修改

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-main.js
+$(function() {
+	var option = {
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 1000,
+		autoplayHoverPause: true
+	};
+	$(".owl-carousel").owlCarousel(option);
+});
