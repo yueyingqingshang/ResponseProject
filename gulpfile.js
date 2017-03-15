ef10888a2e0d0995ab1e33de8de2fdfdea437572
jiangrun <381265457@qq.com>
2017-03-15 18:38:59 +0800

    2017/03/15响应式修改

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var rev = require('gulp-rev');//给每个文件添加版本号
+var filter = require('gulp-filter');//
+var uglify = require('gulp-uglify');//
+var useref = require('gulp-useref');//
+var csso = require('gulp-csso');//
+var revReplace = require('gulp-rev-replace');//
+
+gulp.task('default',function() {
+	var jsFilter = filter('**/*.js',{restore: true});
+	var cssFilter = filter('**/*.css',{restore: true});
+	var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore: true});
+	return gulp.src('src/index.html')
+			.pipe(useref())
+			.pipe(jsFilter)
+			.pipe(uglify())
+			.pipe(jsFilter.restore)
+			.pipe(cssFilter)
+			.pipe(csso())
+			.pipe(cssFilter.restore)
+			.pipe(indexHtmlFilter)
+			.pipe(rev())
+			.pipe(indexHtmlFilter.restore)
+			.pipe(revReplace())
+			.pipe(gulp.dest('dist'));
+});
--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ $(function() {
 		items: 1,
 		loop: true,
 		autoplay: true,
-		autoplayTimeout: 1000,
+		autoplayTimeout: 3000,
 		autoplayHoverPause: true
 	};
 	$(".owl-carousel").owlCarousel(option);
